a9f3caad5800dd6ab2137d65856b43ee8792552c
mike-moran-amd <mike.moran@amd.com>
2022-05-24 13:11:26 -0500

    2 new topologies

--- table/cpuinfo.py
+++ table/cpuinfo.py
@@ -16,7 +16,11 @@ def from_text(text):
         for line in row.split('\n'):
             if line == '':
                 continue
-            col, val = line.split(': ')
+            try:
+                col, val = line.split(': ')
+            except ValueError as err:
+                continue  # the line has no value after the colon (we will call it None)
+
             ret.set_val(row_counter, col.strip(), val)
     return ret
 
@@ -30,4 +34,63 @@ def from_file(filepath='/proc/cpuinfo'):
 class CpuInfoTable(table.Table):
     def __init__(self):
         super().__init__()
-    # TODO add cpuinfo specific methods here ...
+
+    def topology_table(self):
+        tt = TopologyTable()
+        for row in self.row_gen():
+
+            pid = self.get_val(row, 'processor')
+            pid = int(pid)
+            tt.set_val(row, 'PID', pid)
+
+            core_id = self.get_val(row, 'core id')
+            core_id = int(core_id)
+            tt.set_val(row, 'CoreID', core_id)
+
+            socket_id = self.get_val(row, 'physical id')
+            socket_id = int(socket_id)
+            tt.set_val(row, 'SocketID', socket_id)
+
+            # TODO where do we find NUMA node definition
+            tt.set_val(row, 'NUMANodeID', 0)
+
+            # TODO where do we find uncore cache definition
+            tt.set_val(row, 'UnCoreCacheID', 0)
+        return tt
+
+
+class TopologyTable(table.Table):
+    def __init__(self):
+        super().__init__()
+
+    def print_struct(self, name):
+        print(f'    {name} = &topology.CPUTopology' + '{')
+        print(f'        NumCPUs:    {self.num_cpus()},')
+        print(f'        NumSockets: {self.num_sockets()},')
+        print(f'        NumCores:   {self.num_cores()},')
+        print('        CPUDetails: map[int]topology.CPUInfo{')
+        for row in self.row_gen():
+            pid = self.get_val(row, "PID")
+            cid = self.get_val(row, 'CoreID')
+            sid = self.get_val(row, 'SocketID')
+            uid = self.get_val(row, 'UnCoreCacheID')
+            print('            %s: {CoreID: %s, SocketID: %s, UnCoreCacheID: %s},'% (pid, cid, sid, uid))
+        print('        },')
+        print('    }')
+
+    def num_cpus(self):
+        return len(list(self.row_gen()))
+
+    def num_sockets(self):
+        d = {}
+        for row in self.row_gen():
+            socket_id = self.get_val(row, 'SocketID')
+            d[socket_id] = None
+        return len(d.keys())
+
+    def num_cores(self):
+        d = {}
+        for row in self.row_gen():
+            core_id = self.get_val(row, 'CoreID')
+            d[core_id] = None
+        return len(d.keys())
--- table/cpuinfo_test.py
+++ table/cpuinfo_test.py
@@ -3,13 +3,36 @@ import pathlib
 from table import cpuinfo
 
 
-def test_cpuinfo_from_file():
+def get_table(name='cpuinfo_test_epyc_7502p_32'):
     fp = pathlib.Path(__file__).parent
-    fp = pathlib.Path(fp, 'cpuinfo_test_data')
+    fp = pathlib.Path(fp, name)
     t = cpuinfo.from_file(fp)
+    return t
+
+
+def test_cpuinfo_from_file():
+    t = get_table()
     print('\n')
     print(t.pf(title='TABLE'))
     d, s = t.split_static()
     print(s.pf(title='STATIC'))
     print(d.pf(title='DELTA*'))
-    pass
+
+
+def test_initial_apicid_equals_apicid():
+    t = get_table()
+    print('\n')
+
+    for row in t.row_gen():
+        # determine if "apicid" and "initial apicid" are the same
+        val = t.get_val(row, 'apicid')
+        val2 = t.get_val(row, 'initial apicid')
+        assert val == val2
+
+
+def test_generate_topology():
+    t = get_table()
+    print('\n')
+
+    tt = t.topology_table()
+    tt.print_struct('name')
