60eeb23f746b59041ffa0463463f219d97bd3956
Moran <mikmoran@amd.com>
2023-01-17 19:38:24 -0600

    expect testing

--- my_python/expect.py
+++ my_python/expect.py
@@ -17,14 +17,14 @@ class Creds:
     def password_newline_bytes(self):
         return f'{self.__password}\n'.encode(encoding=self.__encoding)
 
-    def pexpect_spawn_ssh(self,
-                          command,
-                          expect_challenge='password:',
-                          child_logfile=None,
-                          child_logfile_read=None,
-                          child_logfile_send=None,
-                          timeout=3,
-                          ):
+    def spawn_ssh(self,
+                  command,
+                  expect_challenge='password:',
+                  child_logfile=None,
+                  child_logfile_read=None,
+                  child_logfile_send=None,
+                  timeout=30,
+                  ):
         ssh_command = f'ssh {self.user_at_host()} {command}'
         child = pexpect.spawn(command=ssh_command, timeout=timeout)
         logging.log(logging.DEBUG, f'pexpect.spawn(command={repr(ssh_command)}, timeout={repr(timeout)}')
@@ -34,7 +34,11 @@ class Creds:
         child.logfile_read = child_logfile_read
         child.logfile_send = child_logfile_send
 
-        pattern = [pexpect.TIMEOUT, expect_challenge]
+        pattern = [
+            pexpect.TIMEOUT,
+            'Are you sure you want to continue connecting (yes/no/[fingerprint])? ',
+            expect_challenge,
+        ]
         logging.log(logging.DEBUG, f'pattern = {pattern}')
 
         expect_ndx = child.expect(pattern=pattern)
@@ -42,13 +46,33 @@ class Creds:
 
         if expect_ndx == 0:
             logging.log(logging.DEBUG, f'child = {child}')
+        elif expect_ndx == 1:
+            child.send(bytes('yes\r'))
+            pass
         else:
             child.send(self.password_newline_bytes())
             logging.log(logging.DEBUG, f'SENT CREDS')
         # end with child, close() sets child exit/signal status
-        return Child(child)
+        return Child(child, encoding=self.__encoding)
 
 
 class Child:
-    def __init__(self, spawn_child):
+    def __init__(self, spawn_child, encoding='utf-8'):
         self.__spawn_child = spawn_child
+        self.__encoding = encoding
+
+    def read(self, size=-1):
+        result_bytes = self.__spawn_child.read(size=size)
+        return result_bytes.decode(encoding=self.__encoding)
+
+    def close(self):
+        self.__spawn_child.close()
+
+    def is_closed(self):
+        return self.__spawn_child.close
+
+    def signal_status(self):
+        return self.__spawn_child.signalstatus
+
+    def status(self):
+        return self.__spawn_child.status
--- my_python/expect_test.py
+++ my_python/expect_test.py
@@ -1,44 +1,34 @@
-import logging
 from my_python import expect
 from pexpect import exceptions
 
-HOSTNAME = 'hostname'
-USERNAME = 'username'
-PASSWORD = 'password'
+TEST_CREDS = expect.Creds(hostname='HOSTNAME', username='USERNAME', password='PASSWORD')
 
 
-def test_pexpect_spawn_ssh__fails_creds(caplog):
-    caplog.set_level(logging.DEBUG)
-    creds = expect.Creds(hostname=HOSTNAME, username=USERNAME, password=PASSWORD)
-    try:
-        child = creds.pexpect_spawn_ssh(command=f'echo "hello world!"')
-        # expecting this to raise, but if not lets see what happened
-        raise RuntimeError(child)
-    except exceptions.EOF as exc:
-        print(f'\nEXCEPTION: {repr(exc)}')
-        print(caplog.text)
-
-
-'''
-def test_pexpect_spawn_ssh__echo_text():
+def test__pexpect_spawn_ssh__echo_text():
     echo_text = 'Hello world!'
-    result, child = expect.pexpect_spawn_ssh(command=f'echo {repr(echo_text)}')
+    child = TEST_CREDS.spawn_ssh(command=f'echo {echo_text}')
+    result = child.read()
+    child.close()
     assert child.signalstatus is None
     assert child.status == 0
-    assert result == bytes(f' \r\n{echo_text}\r\n', encoding='utf-8')
+    assert result == f' \r\n{echo_text}\r\n'
+    pass
+
 
 def test__pexpect_spawn_ssh__fails_mkdir_etc():
-    result, child = expect.pexpect_spawn_ssh(creds=TEST_CREDS, command='mkdir /etc')
+    child = TEST_CREDS.spawn_ssh(command='mkdir /etc')
+    result = child.read()
+    child.close()
     assert child.signalstatus is None
     assert child.status == 256
     assert result == b' \r\nmkdir: cannot create directory \xe2\x80\x98/etc\xe2\x80\x99: File exists\r\n'
+    pass
 
 
 def test__pexpect_spawn_ssh__timeout():
     timeout = 3
     try:
-        result, child = expect.pexpect_spawn_ssh(creds=TEST_CREDS, command=f'sleep {timeout}', timeout=timeout - 1)
+        _ = TEST_CREDS.spawn_ssh(command=f'sleep {timeout}', timeout=timeout - 1)
         raise RuntimeError('EXPECTED EXCEPTION')
-    except exceptions.TIMEOUT as exc:
+    except exceptions.TIMEOUT:
         pass
-'''
