1f72a6293c642c85cfd25eceda1999e3ad877b80
mike-moran-amd <mike.moran@amd.com>
2023-03-28 03:45:12 -0500

    old school pexpect

--- my_python/expect.py
+++ my_python/expect.py
@@ -1,154 +1,101 @@
+import getpass
 import logging
+import socket
+import time
+
 import pexpect
-from pexpect.exceptions import EOF, TIMEOUT
 
 DEFAULT_ENCODING = 'utf-8'
+EOF = pexpect.exceptions.EOF
+TIMEOUT = pexpect.exceptions.TIMEOUT
 
 
 class Creds:
-    def __init__(self, hostname, username, password, encoding='utf-8'):
-        self.__hostname = hostname
-        self.__username = username
+    def __init__(self, password: str, hostname=None, username=None, encoding=None):
         self.__password = password
-        self.__encoding = encoding
+        self.hostname = hostname or socket.gethostname()
+        self.username = username or getpass.getuser()
+        self.encoding = encoding or 'utf-8'
+        self.ssh_password_required = None
 
-    def user_at_host(self):
-        return self.__username + '@' + self.__hostname
+    def spawn(self, command, timeout=30):
+        logging.debug(f'SPAWN: {command}  timeout: {timeout}')
+        return Spawn(pexpect.spawn(command=command, timeout=timeout), creds=self)
 
-    def spawn_ssh(self,
-                  command,
-                  expect_challenge='password:',
-                  timeout=30,
-                  expect_timeout=2,
-                  ):
-
-        ssh_command = f'ssh {self.user_at_host()} {command}'
-        logging.debug(f'pexpect.spawn(command={repr(ssh_command)}, timeout={repr(timeout)}')
-        child = pexpect.spawn(command=ssh_command, timeout=timeout)
-        logging.debug(f'child spawned')
-
-        logging.debug(f'BEFORE {repr(child.before)}')
-        logging.debug(f'AFTER  {repr(child.after)}')
-
-        self_read = None
-        try:
-            ndx = child.expect(pattern=expect_challenge, timeout=expect_timeout)
-            logging.debug(f'EXPECT NDX: {ndx}')
-
-            logging.debug(f'BEFORE {repr(child.before)}')
-            logging.debug(f'AFTER  {repr(child.after)}')
-
-            child.sendline(self.__password)
-            logging.debug('PASSWORD SENT')
-
-        except EOF as eof:
-            logging.debug(f'EXCEPT EOF {eof}')
-            self_read = child.before.decode(encoding=self.__encoding).strip()
-
-        return Child(child, self_read=self_read, encoding=self.__encoding)
-
-    def pexpect_spawn(self, command, timeout=30):
-        logging.debug(f'pexpect.spawn(command={repr(command)}, timeout={repr(timeout)}')
-        child = pexpect.spawn(command=command, timeout=timeout)
-        logging.debug(f'pexpect child spawned')
-
-        return Child(child, self_read=None, creds=self, encoding=self.__encoding)
+    def user_at_host(self) -> str:
+        return self.username + '@' + self.hostname
 
     def get_password(self):
-        logging.debug(f'PASSWORD SENT')
         return self.__password
 
-    def uname(self):
-        child = self.pexpect_spawn(f'ssh {self.user_at_host()} uname -a', timeout=3)
-        child.expect()
-        result = child.read()
-        child.close()
-        logging.debug(f'uname={repr(result)}')
-        return result
+    def ssh_passsword_challenge(self):
+        return f"\r{self.username}@{self.hostname}'s password: "
+
+    def spawn_ssh(self, command='', timeout=-1):
+        ssh_command = f'ssh {self.user_at_host()} {command}'.strip()
+        spawn = self.spawn(ssh_command, timeout=timeout)
+        if self.ssh_password_required in [None, True]:
+            time.sleep(3)
+            ndx = spawn.expect(pattern=[TIMEOUT, self.ssh_passsword_challenge()],  timeout=1)
+            if ndx:
+                logging.debug('PASSWORD REQUIRED')
+                #self.ssh_password_required = True
+            else:
+                logging.debug('PASSWORD NOT REQUIRED')
+                #self.ssh_password_required = False
+        return spawn
 
 
-class Child:
-    def __init__(self, spawn_child, self_read=None, creds=None, encoding='utf-8'):
-        self.__spawn_child = spawn_child
-        self.__self_read = self_read
-        self.__creds = creds
-        self.__encoding = encoding
-        self.__before_read = ''
+class Spawn:
+    """
+    If you need to PIPE, REDIRECT or replace $parameters, then you need to run bash:
+        spawn = Spawn('/bin/bash -c "ls -l | grep LOG > logs.txt"')
+        spawn.expect(pexpect.EOF)
+    """
+    def __init__(self, pexpect_child, creds=None):
+        self.pexpect_child = pexpect_child
+        self.creds = creds
 
-    def read(self, size=-1):
-        if self.__self_read is not None:
-            # if the challenge is not received, everything in the child.before buffer is the result
-            ret = self.__self_read
-            # disable this code path on next read()
-            self.__self_read = None
-            return ret
-        # return read() from child
-        result_bytes = self.__spawn_child.read(size=size)
-        return result_bytes.decode(encoding=self.__encoding).strip()
-
-    def sendline(self, s=''):
-        return self.__spawn_child.sendline(s=s)
-
-    def close(self):
-        self.__spawn_child.close()
-
-    def is_closed(self):
-        return self.__spawn_child.closed
-
-    def signal_status(self):
-        return self.__spawn_child.signalstatus
-
-    def status(self):
-        return self.__spawn_child.status
-
-    def expect(self, pattern=None, expect_timeout=2):
-
-        pattern = pattern or {
-            # pexpect.EOF: None,
-            'password:': lambda: self.sendline(self.__creds.get_password()),
-        }
-        keys = list(pattern.keys())
-
-        logging.debug(f'BEFORE {self.__spawn_child.before}')
-        logging.debug(f'AFTER  {self.__spawn_child.after}')
-
-        ndx = None
-        while not ndx:
-            try:
-                logging.debug('calling expect')
-                ndx = self.__spawn_child.expect(pattern=keys, timeout=expect_timeout)
-                logging.debug(f'EXPECT NDX: {ndx}')
-                key = keys[ndx]
-                fn = pattern[key]
-                if callable(fn):
-                    logging.debug(f'CALLING fn() for challenge: {repr(key)}')
-                    fn()
-                    logging.debug(f'fn() CALLED')
-                elif isinstance(fn, str):
-                    self.sendline(fn)
-                else:
-                    logging.warning(f'UNKNOWN TYPE: {type(fn)}')
-                return True
-            except TIMEOUT:
-                if self.before_endswith('continue connecting (yes/no/[fingerprint])? '):
-                    logging.debug('SENT "yes" to connect')
-                    self.sendline('yes')
-                else:
-                    raise
-            except EOF:
-                logging.debug('The pattern was not seen')
-                # make the next read() get the result
-                self.__self_read = self.before.decode(encoding=self.__encoding).strip()
-                return False
-
-    def before_endswith(self, s):
-        value = self.__spawn_child.before
-        if isinstance(value, bytes):
-            value_str = value.decode(encoding=self.__encoding)
-            if value_str.endswith(s):
-                return True
-        return False
+    def expect(self, pattern, timeout=-1, depth=2):
+        logging.debug(f'EXPECT: {pattern}  timeout: {timeout}')
+        counter = 0
+        while depth - counter > 0:
+            counter += 1
+            ndx = self.pexpect_child.expect(pattern=pattern, timeout=timeout)
+            logging.debug(f'  NDX: {ndx}')
+            if pattern[ndx] == self.creds.ssh_passsword_challenge():
+                logging.debug('GOT SSH PASSWORD CHALLENGE')
+                self.sendline(self.creds.get_password())
+                ndx = self.expect([TIMEOUT, '\r\n'], timeout=1)
+                continue
+            return ndx
 
     @property
     def before(self):
-        return self.__spawn_child.before
+        return self.pexpect_child.before.decode(encoding=self.creds.encoding)
+
+    @property
+    def after(self):
+        return self.pexpect_child
+
+    @property
+    def match(self):
+        # no decode, could be EOF or TIMEOUT
+        return self.pexpect_child.match
+
+    def sendline(self, s=''):
+        logging.debug(f'SENDLINE: {s}')
+        bytes_sent = self.pexpect_child.sendline(s=s)
+        if bytes_sent < len(s):
+            # Only a limited number of bytes may be sent for each line in the default terminal mode
+            logging.warning(f'ONLY {bytes_sent} BYTES SENT OF LENGTH: {len(s)}')
+
+    def read(self, size=-1):
+        bytes_read = self.pexpect_child.read(size=size)
+        logging.debug(f'READ {bytes_read} BYTES')
+        return bytes_read.decode(encoding=self.creds.encoding)
+
+    def get_status(self):
+        self.pexpect_child.close()
+        return self.pexpect_child.status
+        # or?? self.pexpect_child.signalstatus
--- my_python/expect_test.py
+++ my_python/expect_test.py
@@ -1,7 +1,71 @@
+import io
+import logging
 from my_python import expect
-from pexpect import exceptions
+import pexpect
+import sys
 
 TEST_CREDS = expect.Creds(hostname='hostname', username='username', password='password')
+
+
+def print_caplog(x):
+    print('\nCAPLOG:')
+    for line in x.messages:
+        print('\t' + line)
+
+
+def test_old_school(caplog):
+    # using only pexpect commands in a script here, make it ssh to username@hostname, provide password, then run uname
+    spawn = pexpect.spawn('ssh username@hostname', timeout=-1)
+
+    pattern = [
+        f"\rusername@hostname's password: ",
+        pexpect.exceptions.TIMEOUT,
+    ]
+    ndx = spawn.expect(pattern=pattern, timeout=3)
+    if ndx == 0:
+        spawn.sendline('password')
+        spawn.expect('\r\n', timeout=1)
+    ndx = spawn.expect(pattern=pattern, timeout=1)
+    if ndx == 1:
+        raw_text = spawn.before.decode(encoding='utf-8')
+        lines = raw_text.split('\r\n')
+        welcome_to_ubuntu = '\r\n'.join(lines[:-2])
+        assert welcome_to_ubuntu.startswith('Welcome to Ubuntu 20.04.5')
+        prompt = '\r\n' + lines[-1]
+        escaped_prompt = prompt.replace('$', '\\$')
+    else:
+        logging.debug(f'NDX: {ndx}')
+        return
+
+    spawn.sendline('uname -a')
+    spawn.expect(escaped_prompt, timeout=1)
+
+    spawn.sendline('echo $?')
+    spawn.expect(escaped_prompt, timeout=1)
+    assert spawn.before == b'uname -a\r\nLinux u20045 5.4.0-137-generic #154-Ubuntu SMP Thu Jan 5 17:03:22 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux'
+
+    spawn.sendline('exit')
+    spawn.expect(pexpect.exceptions.EOF, timeout=1)
+    assert spawn.before == b'echo $?\r\n0\r\nusername@u20045:~$ exit\r\nlogout\r\nConnection to hostname closed.\r\r\n'
+
+
+def test_creds_spawn_uname(caplog):
+    caplog.set_level(0)  # Everything
+    spawn = TEST_CREDS.spawn('uname -a')
+    spawn.expect(pattern=[expect.EOF], timeout=3)
+    print_caplog(caplog)
+    assert expect.EOF == spawn.match
+    assert 0 == spawn.get_status()
+    print('\n' + spawn.before)
+
+
+def test_creds_spawn_ssh(caplog):
+    caplog.set_level(0)  # Everything
+    spawn = TEST_CREDS.spawn_ssh()
+    spawn.sendline('echo Hello')
+    ndx = spawn.expect(pattern=[expect.TIMEOUT, 'echo Hello'],  timeout=3)
+    assert ndx == 1
+    assert 0 == spawn.get_status()
 
 
 def test_pexpect_spawn_ssh__echo_text(caplog):
@@ -54,9 +118,3 @@ def test_uname(caplog):
     result = TEST_CREDS.uname()
     print(f'\n{repr(result)}')
     print_caplog(caplog)
-
-
-def print_caplog(x):
-    print('\nCAPLOG:')
-    for log in x.messages:
-        print('\t' + log)
